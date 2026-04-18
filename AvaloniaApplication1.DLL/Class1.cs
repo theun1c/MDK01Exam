37c7dea6b1e1d51860cec243b0e0b615d94309a8
theun1c <belovdima1122@yandex.ru>
2026-04-18 06:22:30 +0300

    feat: DLL and TESTS

--- AvaloniaApplication1.DLL/Class1.cs
+++ AvaloniaApplication1.DLL/Class1.cs
@@ -2,4 +2,18 @@
 
 public class Class1
 {
+    public decimal CalculateTotalWeight(List<decimal> weights)
+    {
+        if (weights == null)
+            throw new ArgumentNullException(nameof(weights));
+
+        decimal sum = 0;
+
+        foreach (var weight in weights)
+        {
+            sum += weight;
+        }
+
+        return sum;
+    }
 }
--- AvaloniaApplication1.Test/UnitTest1.cs
+++ AvaloniaApplication1.Test/UnitTest1.cs
@@ -1,10 +1,54 @@
+using AvaloniaApplication1.DLL;
+
 namespace AvaloniaApplication1.Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void CalculateTotalWeight_ReturnsCorrectSum_ForSeveralValues()
     {
-        Assert.True(true);
+        var service = new Class1();
+
+        var result = service.CalculateTotalWeight(new List<decimal> { 1.5m, 2.0m, 3.0m });
+
+        Assert.Equal(6.5m, result);
+    }
+
+    [Fact]
+    public void CalculateTotalWeight_ReturnsZero_ForEmptyList()
+    {
+        var service = new Class1();
+
+        var result = service.CalculateTotalWeight(new List<decimal>());
+
+        Assert.Equal(0m, result);
+    }
+
+    [Fact]
+    public void CalculateTotalWeight_ReturnsSameValue_ForOneElement()
+    {
+        var service = new Class1();
+
+        var result = service.CalculateTotalWeight(new List<decimal> { 4.5m });
+
+        Assert.Equal(4.5m, result);
+    }
+
+    [Fact]
+    public void CalculateTotalWeight_ReturnsCorrectSum_WithZeroValues()
+    {
+        var service = new Class1();
+
+        var result = service.CalculateTotalWeight(new List<decimal> { 0m, 2.5m, 0m });
+
+        Assert.Equal(2.5m, result);
+    }
+
+    [Fact]
+    public void CalculateTotalWeight_ThrowsArgumentNullException_WhenListIsNull()
+    {
+        var service = new Class1();
+
+        Assert.Throws<ArgumentNullException>(() => service.CalculateTotalWeight(null));
     }
 }
