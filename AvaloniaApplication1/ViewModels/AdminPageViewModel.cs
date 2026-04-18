538a53cfe3dcf254790ee61b44568142a69a37d9
theun1c <belovdima1122@yandex.ru>
2026-04-18 06:12:19 +0300

    feat: ADD SEARCH AND SORT

--- AvaloniaApplication1/ViewModels/AdminPageViewModel.cs
+++ AvaloniaApplication1/ViewModels/AdminPageViewModel.cs
@@ -8,13 +8,21 @@ namespace AvaloniaApplication1.ViewModels;
 
 public partial class AdminPageViewModel : ViewModelBase
 {
-    [ObservableProperty] List<Employee> employees = db.Employees
-        .Include(x => x.EmployeeMetrics)
-        .ThenInclude(x => x.Metric)
-        .Include(x => x.Position)
-        .Include(x => x.Role)
-        .ToList();
+    [ObservableProperty] List<Employee> employees = new();
 
+    private List<Employee> allEmployees = new();
+
+    public AdminPageViewModel()
+    {
+        allEmployees = db.Employees
+            .Include(x => x.EmployeeMetrics)
+            .ThenInclude(x => x.Metric)
+            .Include(x => x.Position)
+            .Include(x => x.Role)
+            .ToList();
+        Employees =  allEmployees;
+    }
+    
     public void EditUser(Employee employee)
     {
         MainWindowViewModel.Instance.PageSwitcher = new EditUserPageViewModel(employee);
@@ -28,5 +36,42 @@ public partial class AdminPageViewModel : ViewModelBase
     public void CreateUser()
     {
         MainWindowViewModel.Instance.PageSwitcher = new CreateUserViewModel();
+    }
+
+    [ObservableProperty] private string search;
+
+    partial void OnSearchChanged(string value)
+    {
+        Employees = allEmployees
+            .Where(x => 
+                (x.FirstName.Contains(value)) || 
+                (x.LastName.Contains(value)) ||
+                (x.MiddleName.Contains(value))).ToList();
+    }
+    
+    public void SortDest()
+    {
+        Employees = db.Employees
+            .Include(x => x.Role)
+            .Include(x => x.Position)
+            .Include(x => x.EmployeeMetrics)
+            .ThenInclude(x => x.Metric)
+            .OrderByDescending(x => x.EmployeeMetrics
+                .Select(m => (decimal?)m.Metric.Weight)
+                .Sum() ?? 0)
+            .ToList();
+    }
+
+    public void SortAsc()
+    {
+        Employees = db.Employees
+            .Include(x => x.Role)
+            .Include(x => x.Position)
+            .Include(x => x.EmployeeMetrics)
+            .ThenInclude(x => x.Metric)
+            .OrderBy(x => x.EmployeeMetrics
+                .Select(m => (decimal?)m.Metric.Weight)
+                .Sum() ?? 0)
+            .ToList();
     }
 }
